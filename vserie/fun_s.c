f14822d8ef5267e9e4c60ffe0410a0cd42826ad7
Iyán Álvarez <iyan.22.alvarez@gmail.com>
2020-12-28 14:47:14 +0100

    Variables changed

--- vserie/fun_s.c
+++ vserie/fun_s.c
@@ -30,12 +30,12 @@ double gendist (float *elem1, float *elem2) {
  *          Salida:   popul  grupo mas cercano a cada elemento, vector de tamano MAXE, por referencia      *
  **********************************************************************************************************/
 void grupo_cercano (int nelem, float elem[][NCAR], float cent[][NCAR], int *popul) {
-    int ngrupo;
+    int i, j, ngrupo;
     double adis, dmin;
-    for (int i = 0; i < nelem; i++) {
+    for (i = 0; i < nelem; i++) {
         dmin = DBL_MAX;
-        for (int j = 0; j < NGRUPOS; j++) {
-            adis = gendist(elem[i], cent[j]); // elem[i] o &elem[i][0]
+        for (j = 0; j < NGRUPOS; j++) {
+            adis = gendist(elem[i], cent[j]);
             if (adis < dmin) {
                 dmin = adis;
                 ngrupo = j;
@@ -51,18 +51,20 @@ void grupo_cercano (int nelem, float elem[][NCAR], float cent[][NCAR], int *popu
  *          Salida:   densidad densidad de los grupos (vector de tamano NGRUPOS, por referencia)          *
  **********************************************************************************************************/
 void calcular_densidad (float elem[][NCAR], struct lista_grupos *listag, float *densidad) {
-    for (int i = 0; i < NGRUPOS; i++) {
+    int i, j, k, othg, actg;
+    double acum, cont;
+    for (i = 0; i < NGRUPOS; i++) {
         int nelem = listag[i].nelemg;
         if (nelem < 2) {
             densidad[i] = 0;
         }
         else {
-            int actg;
-            double acum = 0.0, cont = 0.0;
-            for (int j = 0; j < nelem; j++) {
+            acum = 0.0;
+            cont = 0.0;
+            for (j = 0; j < nelem; j++) {
                 actg = listag[i].elemg[j];
-                for (int k = j+1; k < nelem; k++) {
-                    int othg = listag[i].elemg[k];
+                for (k = j+1; k < nelem; k++) {
+                    othg = listag[i].elemg[k];
                     acum += gendist(elem[actg], elem[othg]);
                     cont += 1.0;
                 }
@@ -78,17 +80,19 @@ void calcular_densidad (float elem[][NCAR], struct lista_grupos *listag, float *
  *          Salida:   prob_enf vector de TENF structs (informacion del analisis realizado), por ref.      *
  **********************************************************************************************************/
 void analizar_enfermedades (struct lista_grupos *listag, float enf[][TENF], struct analisis *prob_enf) {
+    int i, j, k, gmax, gmin, nelem, actg;
+    float acum, mediamin, mediamax, mediaact;
     for (int i = 0; i < TENF; i++) {
-        float mediamin = FLT_MAX, mediamax = FLT_MIN;
-        int gmax, gmin;
-        for (int j = 0; j < NGRUPOS; j++) {
-            int nelem = listag[j].nelemg;
-            float acum = 0;
+        mediamin = FLT_MAX;
+        mediamax = FLT_MIN;
+        for (j = 0; j < NGRUPOS; j++) {
+            nelem = listag[j].nelemg;
+            acum = 0;
             for (int k = 0; k < nelem; k++) {
-                int actg = listag[j].elemg[k];
+                actg = listag[j].elemg[k];
                 acum += enf[actg][i];
             }
-            float mediaact = acum/nelem;
+            mediaact = acum/nelem;
             if (mediaact < mediamin) {
                 mediamin = mediaact;
                 gmin = j;
